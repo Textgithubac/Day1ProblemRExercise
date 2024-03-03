def1d1b4fa3c14a5b3d8cb2f4aacb40fe863f6a4
Textgithubac <162041640+Textgithubac@users.noreply.github.com>
2024-03-03 22:03:19 +0530

    Add files via upload

---
+++ TestClass.java
@@ -0,0 +1,15 @@
+package com.Note;
+
+public class TestClass {
+	public static void main(String[] args) {
+        MedicineInfo tablet = new Tablet();
+        MedicineInfo syrup = new Syrup();
+        MedicineInfo ointment = new Ointment();
+        
+        // Check polymorphic behavior of displayLabel()
+        tablet.displayLabel();
+        syrup.displayLabel();
+        ointment.displayLabel();
+    }
+
+}
